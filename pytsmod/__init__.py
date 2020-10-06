c8e5c62ac402bd57deac46d613b0fc134776ed8a
Sangeon Yong <koragon2@kaist.ac.kr>
2020-10-06 20:44:56 +0900

    change version to v0.3.1

--- pytsmod/__init__.py
+++ pytsmod/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.3.0'
+__version__ = '0.3.1'
 
 from .tdpsolatsm import *
 from .wsolatsm import *
--- tests/test_pytsmod.py
+++ tests/test_pytsmod.py
@@ -2,4 +2,4 @@ from pytsmod import __version__
 
 
 def test_version():
-    assert __version__ == '0.3.0'
+    assert __version__ == '0.3.1'
