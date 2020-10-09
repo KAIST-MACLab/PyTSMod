e05c583bdbcdff3e0829d919b32e5383eba72c3e
Sangeon Yong <koragon2@kaist.ac.kr>
2020-10-09 17:46:31 +0900

    add api documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,8 @@
+import os
+import sys
+from pathlib import Path
+
+
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
@@ -12,7 +17,10 @@
 #
 # import os
 # import sys
-# sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../..'))
+# srcpath = os.path.abspath(Path(os.path.dirname(__file__)) / '..' / '..')
+# sys.path.insert(0, srcpath)
+
 
 
 # -- Project information -----------------------------------------------------
@@ -30,8 +38,7 @@ release = '0.3.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = ["sphinx_rtd_theme", "recommonmark", "sphinx.ext.autodoc", "numpydoc"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -47,9 +54,12 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# -- autodoc option ----------------------------------------------------------
+autodoc_member_order = 'bysource'
