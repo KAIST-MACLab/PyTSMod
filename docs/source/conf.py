f191689ba221a1996c354eaf933e9526dfa759e9
Sangeon Yong <koragon2@kaist.ac.kr>
2020-11-24 19:19:22 +0900

    fix index issue for old numpy versions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,7 @@ copyright = '2020, MACLab., KAIST'
 author = 'Sangeon Yong'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3.1'
+release = '0.3.2'
 
 
 # -- General configuration ---------------------------------------------------
