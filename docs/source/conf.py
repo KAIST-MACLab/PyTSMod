078d86a7d573ab5d57fbf89a1f91be6dfd13e1de
Sangeon Yong <koragon2@kaist.ac.kr>
2020-11-24 21:13:09 +0900

    change version to 0.3.3

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,7 @@ copyright = '2020, MACLab., KAIST'
 author = 'Sangeon Yong'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3.2'
+release = '0.3.3'
 
 
 # -- General configuration ---------------------------------------------------
